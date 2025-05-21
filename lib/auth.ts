3f7297dfec701165947d8b823025f4191ab94fb1
Josh Daniel Bañares <joshxfi.dev@gmail.com>
2025-05-21 10:29:20 +0800

    fix: update return session obj

--- lib/auth.ts
+++ lib/auth.ts
@@ -6,7 +6,7 @@ export const getSession = cache(async (): Promise<SessionValidationResult> => {
   const cookieStore = await cookies();
   const token = cookieStore.get("session")?.value ?? null;
   if (token === null) {
-    return { session: null, user: null };
+    return { session: null };
   }
   const result = await validateSessionToken(token);
   return result;
