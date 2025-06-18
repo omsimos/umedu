afa7b9b0e063c600ad344b38fdfb15a644aa0f30
Josh Daniel Bañares <joshxfi.dev@gmail.com>
2025-06-18 12:34:12 +0800

    chore: add trycatch on posts api

--- app/forum/components/feed.tsx
+++ app/forum/components/feed.tsx
@@ -31,11 +31,7 @@ export function Feed() {
     queryKey: ["feed"],
     queryFn: async ({ pageParam }) => {
       const url = pageParam ? `/api/posts?cursor=${pageParam}` : "/api/posts";
-
       const res = await fetch(url);
-      if (!res.ok) {
-        throw new Error("Network response was not ok");
-      }
       return res.json();
     },
     initialPageParam: null,
