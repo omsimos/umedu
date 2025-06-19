37aa42416cc425030dc9f6b6ff8e2c303f8fcb4d
Josh Daniel Bañares <joshxfi.dev@gmail.com>
2025-06-19 22:22:10 +0800

    fix(temp): disable redis cache

--- db/index.ts
+++ db/index.ts
@@ -1,6 +1,5 @@
 import * as schema from "./schema";
 import { drizzle } from "drizzle-orm/libsql";
-import { upstashCache } from "drizzle-orm/cache/upstash";
 
 export const db = drizzle({
   connection: {
@@ -8,14 +7,4 @@ export const db = drizzle({
     authToken: process.env.TURSO_AUTH_TOKEN!,
   },
   schema,
-  ...(process.env.NODE_ENV === "development"
-    ? {}
-    : {
-        cache: upstashCache({
-          url: process.env.UPSTASH_URL!,
-          token: process.env.UPSTASH_TOKEN!,
-          global: true,
-          config: { ex: 60 },
-        }),
-      }),
 });
